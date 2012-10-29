a32c15c4819e66280fe1ad7452a664acb8f3250d
Jay Patel <jayhp9@stanford.edu>
2012-10-29 00:31:53 -0700

    changed to use postgresql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Wave::Application.config.secret_token = 'c9c9045ed2387a59b9b8d5b5d50221c8cd355aa07331bb323f04d0462511f24cf87fcf4b3e8ac6762382e9e63a836d96e0d7c443f9814ea4390af39d5512b3d9'
+Wave::Application.config.secret_token = '7019d462a0e08994f50761b6e81ba514dbccaeca2a1904ae38c264e78cf3bc003c83dcafd5b666a06dfc23b1a413e74d1ff9ce6e412f98b3453f785d792c4eae'
