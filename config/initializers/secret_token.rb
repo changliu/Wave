efadf1b159b5ba43766e6a273b369386946b7393
Jay Patel <jayhp9@stanford.edu>
2012-10-28 22:31:04 -0700

    Created rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wave::Application.config.secret_token = 'c9c9045ed2387a59b9b8d5b5d50221c8cd355aa07331bb323f04d0462511f24cf87fcf4b3e8ac6762382e9e63a836d96e0d7c443f9814ea4390af39d5512b3d9'
