804a3dffff046cc628b306d3c2bc29db89ee0cff
Jay Patel <jayhp9@stanford.edu>
2012-11-15 00:58:57 -0800

    testing

--- app/controllers/soc_mood_picker_controller.rb
+++ app/controllers/soc_mood_picker_controller.rb
@@ -1,6 +1,6 @@
 class SocMoodPickerController < ApplicationController
 	def index
-		access_token = params[:access_token]
+		@access_token = params[:access_token]
 		#@user = User.login(access_token)
 		#session[:user] = @user if !@user.blank?
 		session[:user] = access_token
