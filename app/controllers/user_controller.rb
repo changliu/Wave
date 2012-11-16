38ddc1c93026c51d8da66b7e5190e2e8be7acf69
Jay Patel <jayhp9@stanford.edu>
2012-11-15 22:53:39 -0800

    testing

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,15 @@
 class UserController < ApplicationController
 	def login
-		@access_token = params[:token]
-		if @access_token.blank?
+		#@access_token = params[:token]
+		#if @access_token.blank?
 			#render :status => 500
-		end
-		@user = User.login(access_token)
-		if !@user.blank?
-			session[:user] = @user
+		#end
+		#@user = User.login(access_token)
+		#if !@user.blank?
+		#	session[:user] = @user
 			render :status => 200
-		else
+		#else
 			#render :status => 500
-		end
+		#end
 	end
 end
