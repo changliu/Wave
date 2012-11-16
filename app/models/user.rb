8218792828bce7da8ad2cecbca395d6b7f04e6ab
Jay Patel <jayhp9@stanford.edu>
2012-11-16 02:18:16 -0800

    user log in fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :access_token, :name
   has_and_belongs_to_many :circles
 
-  def self.login(token)	
+  def self.login(token)
+  	return nil if token.blank?
   	user = User.where("access_token = ?", token).first
 		if user.blank?
 			user = User.create!(:access_token => token)
