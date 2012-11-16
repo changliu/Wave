60fc004270a335f8211f8014fdd90e13d5764040
Jay Patel <jayhp9@stanford.edu>
2012-11-15 20:19:59 -0800

    testing circle backend

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,15 @@
 class UserController < ApplicationController
 	def login
-		#@access_token = params[:token]
-		#@user = User.login(access_token)
-		#if !@user.blank?
-		#	session[:user] = @user
-		#	ret = "OK"
-		#else
-		#	ret = "NOK"
-		#end
-		render "hello"
+		@access_token = params[:token]
+		if @access_token is nil
+			render :text => "NOK"
+		end
+		@user = User.login(access_token)
+		if !@user.blank?
+			session[:user] = @user
+			render :text => "OK"
+		else
+			render :text => "NOK"
+		end
 	end
 end
