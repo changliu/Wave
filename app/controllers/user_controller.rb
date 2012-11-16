06dade9bddbbe49936d4b942607aef7d937ce5ba
Jay Patel <jayhp9@stanford.edu>
2012-11-16 01:24:43 -0800

    implemented user log into app

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,17 @@
 class UserController < ApplicationController
 	def login
-		@access_token = params[:token]
-		render :text => @access_token
-		#if @access_token.blank?
-			#render :status => 500
-		#end
-		#@user = User.login(access_token)
-		#if !@user.blank?
-		#	session[:user] = @user
-			#render :status => 200
-		#else
-			#render :status => 500
-		#end
+		access_token = params[:token]
+		if access_token.blank?
+			render :text => "NOK"
+			return
+		end
+
+		user = User.login(access_token)
+		if !user.blank?
+			session[:user] = user
+			render :text => "OK"
+		else
+			render :text => "NOK"
+		end
 	end
 end
