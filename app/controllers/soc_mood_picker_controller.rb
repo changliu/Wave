4ad69c92a844993ca75a0e7b7b3280df81a4a5b6
Jay Patel <jayhp9@stanford.edu>
2012-11-29 16:58:43 -0800

    done implementing circle backend stuff

--- app/controllers/soc_mood_picker_controller.rb
+++ app/controllers/soc_mood_picker_controller.rb
@@ -1,5 +1,4 @@
 class SocMoodPickerController < ApplicationController
 	def index
-		
 	end
 end
--- app/controllers/soc_wave_list_controller.rb
+++ app/controllers/soc_wave_list_controller.rb
@@ -1,2 +1,10 @@
 class SocWaveListController < ApplicationController
+	def login
+		dest = params[:dest]
+		if dest == "groups"
+			session[:dest] = "groups"
+		else
+			session[:dest] = "moods"
+		end
+	end
 end
